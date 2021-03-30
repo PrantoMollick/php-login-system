2e39046fbd0d5a611c607564afae3c6b76e61c18
Pranto Mollick <prantocse01@gmail.com>
2021-03-30 14:19:52 +0600

    Added login form

--- index.php
+++ index.php
@@ -1,3 +1,55 @@
-<?php
-echo "Hello World!";
-?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.18/css/uikit.min.css" integrity="sha512-UmBu02JHsKZr8Bzy5IqX1QE5qgIe8RuKDVMc27tYNzx8wBWKbIWz8p2/oDdoVBW6zM2O/63juzUYXu8oV93Lwg==" crossorigin="anonymous" />
+    <title>Document</title>
+</head>
+<body>
+
+
+    <div class="uk-section uk-container">
+        <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+            <form class="uk-form-stacked js-login">
+
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-horizontal-text">Email</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-horizontal-text" type="email" required="required" placeholder="email@email.com">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-horizontal-text">Email</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-horizontal-text" type="password" required="required" placeholder="Enter your password">
+                    </div>
+                </div>
+
+                <div class="uk-margin">
+                    <button class="uk-button uk-button-default" type="submit">LogIn</button>
+
+                </div>
+
+            </form>
+
+        </div>
+    </div>
+
+
+
+
+    <!--jQuery is required-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+
+    <!--UIkit js-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.18/js/uikit.min.js" integrity="sha512-C4F3t1T3+jmG6ZtaYGOuh7lFRD5FrsfE86LfPXYJRvG7Fx2+5UONOJHY8dor6vXzPl9kqcfH32MuwZifYEShNA==" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.6.18/js/uikit-icons.min.js" integrity="sha512-hdsnoICNClpajk3gXuVwzaxkF381MvG77GJpTTjXhxhqKxGzk4kEPXnFWCx2M1S0rso8uoKntFbU/Hk6gJeaMQ==" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
